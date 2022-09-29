f7b93a07029556a4716ce3076d4fbf3b86857c40
Salief Lewis <salieflewis@gmail.com>
2022-09-29 10:09:49 -0400

    remove: socials component

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,9 +1,5 @@
-import { Socials } from './Socials'
-
 export function Footer() {
   return (
-    <footer className="flex flex-row justify-between items-center w-full px-8">
-      <Socials />
-    </footer>
+    <footer className="flex flex-row justify-between items-center w-full px-8"></footer>
   )
 }
