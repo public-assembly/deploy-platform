3bed0a608e801e530c5995ac87fb5a89bcdd2983
Salief Lewis <salieflewis@gmail.com>
2022-10-01 17:31:01 -0400

    context config

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,16 @@
 import 'styles/globals.css'
 import type { AppProps } from 'next/app'
 import { AppWrapper, PageWrapper } from './../components'
+import { FormStateProvider } from '../context'
 
 function DeployPlatform({ Component, pageProps }: AppProps) {
   return (
     <AppWrapper>
-      <PageWrapper>
-        <Component {...pageProps} />
-      </PageWrapper>
+      <FormStateProvider>
+        <PageWrapper>
+          <Component {...pageProps} />
+        </PageWrapper>
+      </FormStateProvider>
     </AppWrapper>
   )
 }
