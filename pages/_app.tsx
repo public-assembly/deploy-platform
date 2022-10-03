fc071e0ee4809b82edade8c4d062aa9335c62ab1
Salief Lewis <salieflewis@gmail.com>
2022-10-03 12:36:36 -0400

    defined typography styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import 'styles/globals.css'
+import 'styles/publicassembly.css'
 import type { AppProps } from 'next/app'
 import { AppWrapper, PageWrapper } from './../components'
 import { FormStateProvider } from '../context'
