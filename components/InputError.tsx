e2ca9c1d3a15c270a1c1c75c6a7327278a7c0161
Salief Lewis <salieflewis@gmail.com>
2022-10-27 14:36:24 -0400

    remove: mobile adjustment for alerts

--- components/InputError.tsx
+++ components/InputError.tsx
@@ -1,6 +1,6 @@
 export const InputError = ({ errorMessage }: { errorMessage?: string }) => {
   return (
-    <div className="items-center text-[#8A2C26] max-w-md gap-2 text-lg sm:text-xl bg-[#F3D5D5] px-4 py-2 font-normal rounded-lg absolute bottom-80 sm:top-6 sm:right-6">
+    <div className="items-center text-[#8A2C26] max-w-md gap-2 text-lg sm:text-xl bg-[#F3D5D5] px-4 py-2 font-normal rounded-lg absolute top-6 right-6">
       {errorMessage}
     </div>
   )
